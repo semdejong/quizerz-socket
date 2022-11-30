ee6e842fdde1bbdf954019efebcb562220f53555
Sem de Jong <semdejo2003@gmail.com>
2022-11-30 02:38:38 +0100

    reworked for https

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-require("dotenv").config();
-const { v4: uuidv4 } = require("uuid");
-const https = require("https");
-const fs = require("fs");
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import https from "https";
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
+import fetch from "node-fetch";
+import fs from "fs";
 
-const express = require("express");
 const app = express();
 
 var httpsOptions = {
@@ -13,7 +16,6 @@ var httpsOptions = {
 };
 
 const server = https.createServer(httpsOptions, app);
-const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
@@ -25,129 +27,160 @@ const io = new Server(server, {
 let players = [];
 let games = [];
 
-io.on("connection", (socket) => {
-  socket.on("join-game", (userName, pin) => {
-    console.log("join-game", pin, userName);
-    const game = games.find((game) => game.pin === pin);
-    if (!game) {
-      socket.emit("response-join-game", false, "Game not found");
-      return;
-    }
-
-    if (game.players.find((player) => player.name === userName)) {
-      socket.emit("response-join-game", false, "Username already taken");
-      return;
-    }
-
-    const clientSecret = uuidv4();
-
-    const player = {
-      id: socket.id,
-      name: userName,
-      joinedGame: game.id,
-      pin: pin,
-      clientSecret,
-    };
-
-    players.push(player);
-
-    games
-      .find((game) => game.id === player.joinedGame)
-      .players.push({ ...player, clientSecret: undefined });
-
-    socket.join(game.id);
-
-    const hostSocket = io.sockets.sockets.get(game.host.id);
-
-    hostSocket.emit("player-joined", game.players);
-
-    socket.emit("response-join-game", true, player);
-  });
-
-  socket.on("check-pin-exists", (pin) => {
-    const game = games.find((game) => game.pin === pin);
-    if (game) {
-      socket.emit("reponse-check-pin-exists", pin);
-    } else {
-      socket.emit("reponse-check-pin-exists", false);
-    }
-  });
-
-  socket.on("create-game", (gameId, clientSecret = "") => {
-    let game = {};
-    const gameExists = games.find((game) => game.id === gameId);
-    if (gameExists) {
-      if (gameExists.host.secret === clientSecret) {
-        socket.emit("game-exists", { ...gameExists, host: false });
-      } else {
-        console.log("game exists");
-        socket.emit("game-exists", false);
+try {
+  io.on("connection", (socket) => {
+    socket.on("join-game", (userName, pin) => {
+      console.log("join-game", pin, userName);
+      const game = games.find((game) => game.pin === pin);
+      if (!game) {
+        socket.emit("response-join-game", false, "Game not found");
+        return;
       }
-    } else {
+
+      if (game.players.find((player) => player.name === userName)) {
+        socket.emit("response-join-game", false, "Username already taken");
+        return;
+      }
+
       const clientSecret = uuidv4();
-      const pin = makeid(6).toUpperCase();
-      game = {
-        id: gameId,
-        host: { id: socket.id, secret: clientSecret },
+
+      const player = {
+        id: socket.id,
+        name: userName,
+        joinedGame: game.id,
         pin: pin,
-        players: [],
-        currentPage: "lobby",
+        clientSecret,
       };
-      games.push(game);
-      socket.emit("game-created", game, clientSecret);
-      socket.join(gameId);
-    }
-  });
 
-  socket.on("set-host", (gameId, clientSecret) => {
-    console.log("SETHOST");
-    const game = games.find((game) => game.id === gameId);
-    if (game.host.secret === clientSecret) {
-      game.host.id = socket.id;
-      socket.emit("host-set", game);
-    } else {
-      socket.emit("host-set", false);
-    }
-  });
+      players.push(player);
 
-  socket.on("rejoin-game", (clientSecret) => {
-    const player = players.find(
-      (player) => player.clientSecret === clientSecret
-    );
-    if (player) {
-      player.id = socket.id;
-      socket.join(player.joinedGame);
-      const game = games.find((game) => game.id === player.joinedGame);
-      game.players.push(player);
+      games
+        .find((game) => game.id === player.joinedGame)
+        .players.push({ ...player, clientSecret: undefined });
+
+      socket.join(game.id);
+
       const hostSocket = io.sockets.sockets.get(game.host.id);
-      hostSocket.emit("player-joined", game.players);
-      socket.emit("response-rejoin-game", player, game.currentPage);
-    } else {
-      socket.emit("response-rejoin-game", false);
-    }
-  });
 
-  socket.on("next-question", (questionId, gameId) => {
-    const game = games.find((game) => game.id === gameId);
-    game.currentPage = "question";
-    if (game) {
-      socket.to(gameId).emit("next-question", questionId);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    const player = players.find((player) => player.id === socket.id);
-    if (player) {
-      const game = games.find((game) => game.id === player.joinedGame);
-      if (game) {
-        game.players = game.players.filter((player) => player.id !== socket.id);
-        const hostSocket = io.sockets.sockets.get(game.host.id);
-        hostSocket.emit("player-left", game.players);
+      if (hostSocket) {
+        hostSocket.emit("player-joined", game.players);
       }
-    }
-    console.log("user disconnected");
+      socket.emit("response-join-game", true, player, {
+        ...game?.questions?.[game.currentQuestion],
+        correctAnswer: undefined,
+      });
+    });
+
+    socket.on("check-pin-exists", (pin) => {
+      const game = games.find((game) => game.pin === pin);
+      if (game) {
+        socket.emit("reponse-check-pin-exists", pin);
+      } else {
+        socket.emit("reponse-check-pin-exists", false);
+      }
+    });
+
+    socket.on("create-game", async (gameId, clientSecret = "") => {
+      let game = {};
+      const gameExists = games.find((game) => game.id === gameId);
+      if (gameExists) {
+        if (gameExists.host.secret === clientSecret) {
+          socket.emit("game-exists", { ...gameExists, host: false });
+        } else {
+          console.log("game exists");
+          socket.emit("game-exists", false);
+        }
+      } else {
+        const gameData = await getGameData(gameId);
+
+        if (gameData.result.status !== 200) {
+          socket.emit("game-exists", false);
+          return;
+        }
+
+        const clientSecret = uuidv4();
+        const pin = makeid(6).toUpperCase();
+        game = {
+          id: gameId,
+          host: { id: socket.id, secret: clientSecret },
+          pin: pin,
+          players: [],
+          currentPage: "lobby",
+          gameData: { ...gameData.data, questions: undefined },
+          questions: gameData.data.questions,
+          currentQuestion: -1,
+        };
+        games.push(game);
+        socket.emit("game-created", game, clientSecret);
+        socket.join(gameId);
+      }
+    });
+
+    socket.on("set-host", (gameId, clientSecret) => {
+      console.log("SETHOST");
+      const game = games.find((game) => game.id === gameId);
+      if (game?.host?.secret === clientSecret) {
+        game.host.id = socket.id;
+        socket.emit("host-set", game);
+      } else {
+        socket.emit("host-set", false);
+      }
+    });
+
+    socket.on("rejoin-game", (clientSecret) => {
+      const player = players.find(
+        (player) => player.clientSecret === clientSecret
+      );
+      if (player) {
+        player.id = socket.id;
+        socket.join(player.joinedGame);
+        const game = games.find((game) => game.id === player.joinedGame);
+        game.players.push(player);
+        const hostSocket = io.sockets.sockets.get(game.host.id);
+        if (hostSocket) {
+          hostSocket.emit("player-joined", game.players);
+        }
+        socket.emit("response-rejoin-game", player, game.currentPage, {
+          ...game?.questions?.[game.currentQuestion],
+          correctAnswer: undefined,
+        });
+      } else {
+        socket.emit("response-rejoin-game", false);
+      }
+    });
+
+    socket.on("next-question", (questionId, gameId) => {
+      const game = games.find((game) => game.id === gameId);
+      game.currentQuestion = game.currentQuestion + 1;
+      game.currentPage = "question";
+      if (game) {
+        socket.to(gameId).emit("next-question", {
+          ...game?.questions?.[game.currentQuestion],
+          correctAnswer: undefined,
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      const player = players.find((player) => player.id === socket.id);
+      if (player) {
+        const game = games.find((game) => game.id === player.joinedGame);
+        if (game) {
+          game.players = game.players.filter(
+            (player) => player.id !== socket.id
+          );
+          const hostSocket = io.sockets.sockets.get(game.host.id);
+          if (hostSocket) {
+            hostSocket.emit("player-left", game.players);
+          }
+        }
+      }
+      console.log("user disconnected");
+    });
   });
-});
+} catch (e) {
+  console.log(e);
+}
 
 server.listen(process.env.PORT, () => {
   console.log("listening on *:" + process.env.PORT);
